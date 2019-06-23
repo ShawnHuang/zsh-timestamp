ff281324a684f7e916a5fd0cdfe5558bd340c3ba
Shawn Huang <Shawn.Huang@quantatw.com>
2019-06-23 09:58:19 +0800

    First init.
    Signed-off-by: Shawn Huang <Shawn.Huang@quantatw.com>

---
+++ timestamp.zsh
@@ -0,0 +1,26 @@
+fpath+="${0:h}/src"
+
+del-time-prompt-accept-line() {
+   zle accept-line
+}
+zle -N del-time-prompt-accept-line
+bindkey "^M" del-time-prompt-accept-line
+
+function preexec() {
+  timer=`date '+%H:%M:%S'`
+  time_set="1"
+}
+
+function precmd() {
+  if [ $time_set ]; then
+    RPROMPT='[${timer}]-[%*]'
+  else
+    RPROMPT='[%*]'
+    unset timer
+  fi
+  unset time_set
+}
+
+autoload -Uz add-zsh-hook
+add-zsh-hook preexec preexec
+add-zsh-hook precmd precmd
