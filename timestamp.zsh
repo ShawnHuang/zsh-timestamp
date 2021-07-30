569fad4b664f73512a3f641bcc8e96104387029f
Shawn Huang <Shawn.Huang@mail>
2021-07-30 09:04:50 +0800

    update
    Signed-off-by: Shawn Huang <Shawn.Huang@mail>

--- timestamp.zsh
+++ timestamp.zsh
@@ -1,17 +1,19 @@
-del-time-prompt-accept-line() {
-   zle accept-line
-}
-zle -N del-time-prompt-accept-line
-bindkey "^M" del-time-prompt-accept-line
+#
+#del-time-prompt-accept-line() {
+#   zle accept-line
+#}
+#zle -N del-time-prompt-accept-line
+#bindkey "^M" del-time-prompt-accept-line
 
 function preexec() {
   timer=`date '+%H:%M:%S'`
   time_set="1"
+  RPROMPT='[${timer} - %*]'
 }
 
 function precmd() {
   if [ $time_set ]; then
-    RPROMPT='[${timer}]-[%*]'
+    RPROMPT='[${timer} - %*]'
   else
     RPROMPT='[%*]'
     unset timer
@@ -22,3 +24,8 @@ function precmd() {
 autoload -Uz add-zsh-hook
 add-zsh-hook preexec preexec
 add-zsh-hook precmd precmd
+
+
+# rgs() {
+#   eval "$(fc -l -n -1) -l" | tee >(fzf-tmux -d 15 | xclip -selection clipboard) > /dev/null && tmux set-buffer $(xclip -o -sel clipboard)
+# }
